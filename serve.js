70544ac1b73738b764493fee0888694d82f48664
Aodhán Moran <aodhanmoran@Aodhans-MacBook-Pro-Biggy.local>
2025-01-28 14:47:01 +0000

    Shit loads of changes

--- serve.js
+++ serve.js
@@ -4,22 +4,22 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files from the public directory
-app.use(express.static('public'));
+// Serve static files from the src directory
+app.use(express.static('src'));
 
 // Handle routes for HTML files
 app.get('/:page', (req, res) => {
     const page = req.params.page;
-    res.sendFile(path.join(__dirname, 'public', `${page}.html`), (err) => {
+    res.sendFile(path.join(__dirname, 'src', `${page}.html`), (err) => {
         if (err) {
-            res.sendFile(path.join(__dirname, 'public', 'index.html'));
+            res.sendFile(path.join(__dirname, 'src', 'index.html'));
         }
     });
 });
 
 // Root route
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'src', 'index.html'));
 });
 
 app.listen(PORT, () => {
